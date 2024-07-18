c086fb95ce750cdbbbe1139cfc3be1348d4086c9
Brian <brianmorales395@yahoo.com>
2024-07-17 22:57:39 -0400

    Edited Lualine Plugin

--- lua/plugins/lualine-nvim.lua
+++ lua/plugins/lualine-nvim.lua
@@ -1,16 +1,16 @@
 local config = function()
 	require("lualine").setup({
 		options = {
-			theme = "gruvbox-material",
+			theme = "moonfly",
 			globalstatus = true,
 			--	component_separators = { left = "|", right = "|" },
 			--	section_separators = { left = "", right = "" },
-			section_separators = { left = "", right = "" },
+			-- section_separators = { left = "", right = "" },
 			component_separators = { left = "|", right = "|" },
 		},
 		sections = {
 			lualine_a = { "mode" },
-			--	lualine_b = { "buffers" },
+			lualine_b = { "buffers" },
 			lualine_c = { "filename" },
 			lualine_x = { "encoding", "fileformat", "filetype" },
 			lualine_y = { "progress" },
