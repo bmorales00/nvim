b9da234bdf9467ed9634a234c135004948a587bc
Brian <brianmorales395@yahoo.com>
2024-07-18 11:03:49 -0400

    Edited lualine-nvim.lua

--- lua/plugins/lualine-nvim.lua
+++ lua/plugins/lualine-nvim.lua
@@ -10,7 +10,7 @@ local config = function()
 		},
 		sections = {
 			lualine_a = { "mode" },
-			lualine_b = { "buffers" },
+			lualine_b = { "branch", "diff" },
 			lualine_c = { "filename" },
 			lualine_x = { "encoding", "fileformat", "filetype" },
 			lualine_y = { "progress" },
