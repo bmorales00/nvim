f9a40665df083e9a7120579a30801642bd41f139
Brian <brianmorales395@yahoo.com>
2024-07-25 15:34:29 -0400

    Added wl-clipboard in globals.lua

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -1,2 +1,15 @@
 vim.g.mapleader = " ";
 vim.g.maplocalleader = " ";
+
+vim.g.clipboard ={
+  name = 'wl-clipboard',
+  copy = {
+    ['+'] = 'wl-copy',
+    ['*'] = 'wl-copy',
+  },
+  paste = {
+    ['+'] = 'wl-paste',
+    ['*'] = 'wl-paste',
+  },
+  cache_enabled = 1,
+};
