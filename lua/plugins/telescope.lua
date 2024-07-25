a0d3b3e8fda5450b99104856628cd568e8019514
Brian <brianmorales395@yahoo.com>
2024-07-25 15:39:10 -0400

    Added new keymaps to telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -54,8 +54,7 @@ return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.3",
 	lazy = false,
-	dependencies = { { "nvim-lua/plenary.nvim" }, 
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
+	dependencies = { { "nvim-lua/plenary.nvim" }, { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
 	config = config,
 	keys = keys,
 }
