138c4b581a86aa6752ed6562b2a00e62e940f9a4
Brian <brianmorales395@yahoo.com>
2024-07-24 23:33:49 -0400

    Added a color set to mellifluous.lua

--- lua/plugins/mellifluous.lua
+++ lua/plugins/mellifluous.lua
@@ -1,25 +1,44 @@
 return {
-  "ramojus/mellifluous.nvim",
-  version = "v0.*",
-  priority = 1000,
-  lazy = false,
-  config = function()
-    require("mellifluous").setup({
-      transparent_background = {
-        enabled = true,
-      },
-      plugins = {
-        nvim_tree = {
-          enabled = true,
-          show_root = true,
-        },
-      },
-      mellifluous = {
-        neutral = true,
-        bg_contrast = "soft",
-      },
-    })
-    vim.cmd("colorscheme mellifluous")
-  end,
+	"ramojus/mellifluous.nvim",
+	version = "v0.*",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		require("mellifluous").setup({
+			dim_inactive = false,
+			color_set = "mellifluous",
+			flat_background = {
+				line_numbers = true,
+				floating_windows = true,
+				file_tree = true,
+				cursor_line_number = true,
+			},
+			plugins = {
+				cmp = true,
+				gitsigns = true,
+				indent_blankline = true,
+				nvim_tree = {
+					enabled = false,
+					show_root = false,
+				},
+				neo_tree = {
+					enabled = false,
+				},
+				telescope = {
+					enabled = true,
+					nvchad_like = false,
+				},
+			},
+			mellifluous = {
+				color_overrides = {
+					dark = {
+						bg = "#080808",
+					},
+				},
+				neutral = true,
+				bg_contrast = "hard",
+			},
+		})
+		vim.cmd("colorscheme mellifluous")
+	end,
 }
-
