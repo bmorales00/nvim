85b561cec919cc57de4db834961a94f04f413734
Brian <brianmorales395@yahoo.com>
2024-10-10 15:47:02 -0400

    Removed unused plugins

--- lua/plugins/nvim-dap-ui.lua
+++
@@ -1,5 +0,0 @@
--- UI for nvim-dap
-return {
-	"rcarriga/nvim-dap-ui",
-	dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-}
--- lua/plugins/true-zen.lua
+++
@@ -1,5 +0,0 @@
--- True Zen is a clean and distraction-free UI for Neovim.
-return {
-	"pocco81/true-zen.nvim",
-	lazy = false,
-}
