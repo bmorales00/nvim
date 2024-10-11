bffb3dc4f5866729950206cc35a148a2d4814a16
Brian <brianmorales395@yahoo.com>
2024-10-11 17:07:49 -0400

    Added bufferline plugin and add recfactor motion in lspsaga

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,27 @@
+return {
+	"akinsho/bufferline.nvim",
+	tag = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	lazy = false,
+	config = function()
+		vim.opt.termguicolors = true
+		require("bufferline").setup({
+
+			options = {
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = " NvimTree Explorer",
+						separator = true,
+						text_align = "left",
+					},
+				},
+				diagnostics = "nvim_lsp",
+				separator_style = { "", "" },
+				modified_icon = "●",
+				show_close_icon = false,
+				show_buffer_close_icons = false,
+			},
+		})
+	end,
+}
