94820cf5449dc4756aaabf4372b4a09716f7345b
Brian <brianmorales395@yahoo.com>
2024-11-08 00:18:58 -0500

    Changed togterm float value

--- lua/plugins/togterm.lua
+++ lua/plugins/togterm.lua
@@ -3,5 +3,15 @@ return {
 	"akinsho/toggleterm.nvim",
 	lazy = false,
 	version = "*",
-	config = true,
+	config = function()
+		require("toggleterm").setup({
+			open_mapping = [[<c-\>]],
+			shade_terminals = false,
+			shell = "zsh --login",
+		})
+	end,
+	keys = {
+		{ [[<C-\>]] },
+		{ "<leader>cf", "<cmd>ToggleTerm siize=40 direction=float<cr>", desc = "Open Float Terminal" },
+	},
 }
