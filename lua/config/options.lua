26c15eadc22283de961d9ffd52671d0885678bac
Brian <brianmorales395@yahoo.com>
2025-06-13 15:55:27 -0400

    set conceal level to 1

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -46,3 +46,4 @@ opt.guicursor =
 	"n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 opt.encoding = "UTF-8"
 opt.showmode = false
+opt.conceallevel = 1
