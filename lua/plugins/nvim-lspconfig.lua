7821e7297a3a92f2487ad925e2f77f1d445bed60
Brian <brianmorales395@yahoo.com>
2025-09-22 18:29:43 -0400

    added vue lsp and changed ts_ls to vtsls

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -49,12 +49,91 @@ local config = function()
 		filetypes = { "json", "jsonc" },
 	})
 
-	----------------------------------- typescript
-	lspconfig.ts_ls.setup({
+	----------------------------------- vtsls (ts)
+	--- vue_ls path
+	local vue_language_server_path = vim.fn.stdpath("data")
+		.. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+
+	lspconfig.vtsls.setup({
 		on_attach = on_attach,
 		capabilities = capabilities,
 		filetypes = {
 			"typescript",
+			"javascript",
+			"vue",
+		},
+		settings = {
+			vtsls = {
+				tsserver = {
+					globalPlugins = {
+						{
+							name = "@vue/typescript-plugin",
+							location = vue_language_server_path,
+							languages = { "vue" },
+							configNamespace = "typescript",
+						},
+					},
+				},
+			},
+		},
+	})
+
+	----------------------------------- vue_ls (volar)
+
+	--- works in hybrid mode alongside vtsls
+	--- When you are editing the <script> block of a .vue file, Volar forwards the code and your actions (like asking for autocompletion)
+	--- to the vtsls server. vtsls processes it and sends the result back to Volar, which then displays it in your editor.
+
+	lspconfig.volar.setup({
+		on_init = function(client)
+			client.handlers["tsserver/request"] = function(_, result, context)
+				local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
+				if #clients == 0 then
+					vim.notify(
+						"Could not find `vtsls` lsp client, vue_lsp will not work without it!",
+						vim.log.levels.ERROR
+					)
+					return
+				end
+				local ts_client = clients[1]
+
+				local param = unpack(result)
+				local id, command, payload = unpack(param)
+				ts_client:exec_cmd({
+					title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+					command = "typescript.tsserverRequest",
+					arguments = {
+						command,
+						payload,
+					},
+				}, { bufnr = context.bufnr }, function(_, r)
+					local response_data = { { id, r.body } }
+					---@diagnostic disable-next-line: param-type-mismatch
+					client:notify("tsserver/response", response_data)
+				end)
+			end
+		end,
+		settings = {
+			typescript = {
+				inlayHints = {
+					enumMemberValues = {
+						enabled = true,
+					},
+					functionLikeReturnTypes = {
+						enabled = true,
+					},
+					propertyDeclarationTypes = {
+						enabled = true,
+					},
+					parameterTypes = {
+						enabled = true,
+						suppressWhenArgumentMatchesName = true,
+					},
+					variableTypes = {
+						enabled = true,
+					},
+				},
+			},
 		},
 	})
 
