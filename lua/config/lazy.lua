0d5e7b6453c5d2e30ca1cba84e01ae53fc07d12c
Brian <brianmorales395@yahoo.com>
2025-10-02 13:43:48 -0400

    Fixed: Added lazy update checker

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -2,16 +2,19 @@
 -- This file is responsible for setting up the configuration
 -- This is based on the lazy.nvim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
+---@diagnostic disable-next-line: undefined-field (fs_stat)
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -47,6 +50,9 @@ local opts = {
 	},
 	ui = {
 		border = "rounded",
+	},
+	checker = {
+		enabled = true,
 	},
 }
 
