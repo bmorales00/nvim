e4262103239cd8c053d348a67ebfbf9acdbdb6c9
Brian <brianmorales395@yahoo.com>
2025-10-02 13:21:33 -0400

    Fixed: Yank automcmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -17,8 +17,12 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 local highlight_yank_group = vim.api.nvim_create_augroup("HighlightYankGroup", {})
 vim.api.nvim_create_autocmd("TextYankPost", {
 	group = highlight_yank_group,
+  pattern = "*",
 	callback = function()
-		vim.highlight.on_yank()
+    vim.hl.on_yank({
+      higroup = "IncSearch",
+      timeout = 200
+    })
 	end,
 })
 
