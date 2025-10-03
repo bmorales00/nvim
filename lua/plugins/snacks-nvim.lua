8529c0ac89a49193769c67f11aa433bb27d9cf05
Brian <brianmorales395@yahoo.com>
2025-10-03 13:14:41 -0400

    Added: plugins that replace existing ones

--- lua/plugins/snacks-nvim.lua
+++ lua/plugins/snacks-nvim.lua
@@ -1,28 +1,29 @@
 -- lazy.nvim
 return {
 	"folke/snacks.nvim",
+	priority = 1000,
 	lazy = false,
 	---@type snacks.Config
-	config = function()
-		require("snacks").setup({
-			styles = {
-				snacks_image = {
-					relative = "editor",
-					col = -1,
-				},
+	opts = {
+		-- Core Modeules
+		dashboard = { enabled = true },
+
+		-- QoL Modules
+		zen = { enabled = true },
+		indent = { enabled = true },
+
+		-- Image
+		image = { enabled = true },
+		image = { max_height = 20, doc = { inline = false, float = true } },
+
+		-- Styles for modules
+		styles = {
+			snacks_image = {
+				relative = "editor",
+				col = -1,
+				row = -1,
+				border = "rounded",
 			},
-			image = {
-				enabled = true,
-				doc = {
-					inline = false,
-					float = true,
-				},
-			},
-			opts = {
-				-- your image configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			},
-		})
-	end,
+		},
+	},
 }
