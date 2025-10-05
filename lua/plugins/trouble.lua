b4101de181092e03ec8835eb989e564fd17e0ee9
Brian <brianmorales395@yahoo.com>
2025-10-04 23:46:07 -0400

    Fixed: rewrite of trouble plugin

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,32 +1,38 @@
 -- Trouble is a plugin that shows diagnostics, references, telescope results, quickfix and location lists to help you solve all the trouble your code is causing.
-local diagnostics_signs = require("util.icons").debugging_signs
-local maplazykey = require("util.keymapper").maplazykey
 return {
 	"folke/trouble.nvim",
-  lazy =false,
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>cs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
 	},
-  keys = {
-      maplazykey("<leader>xx", function()
-        require("trouble").toggle()
-      end, "Toggle Trouble"),
-      maplazykey("<leader>xw", function()
-        require("trouble").toggle("workspace_diagnostics")
-      end, "Show Workspace Diagnostics"),
-      maplazykey("<leader>xd", function()
-        require("trouble").toggle("document_diagnostics")
-      end, "Show Document Diagnostics"),
-      maplazykey("<leader>xq", function()
-        require("trouble").toggle("quickfix")
-      end, "Toggle Quickfix List"),
-      maplazykey("<leader>xl", function()
-        require("trouble").toggle("loclist")
-      end, "Toggle Location List"),
-      maplazykey("gR", function()
-        require("trouble").toggle("lsp_references")
-      end, "Toggle LSP References"),
-    },
 }
