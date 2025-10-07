deded1068c0e0d25c14b5a41ecb1c4f0d55875f6
Brian <brianmorales395@yahoo.com>
2025-10-07 15:03:57 -0400

    Fixed: dependency issues along with typo

--- lua/plugins/render-md.lua
+++ lua/plugins/render-md.lua
@@ -1,6 +1,6 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
 	-- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
 	---@module 'render-markdown'
@@ -8,7 +8,7 @@ return {
 	opts = {},
 	config = function()
 		require("render-markdown").setup({
-			completion = { lsp = { enabled = true } },
+			completions = { lsp = { enabled = true } },
 		})
 	end,
 }
