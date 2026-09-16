a9288fa358b83afb4e6f5fdfdb7ae1f45ac35f19
Brian <git.services@brianbmm.com>
2026-09-16 11:52:56 -0400

    Updated UI2 to display messages on the top right

--- lua/config/ui2.lua
+++ lua/config/ui2.lua
@@ -4,10 +4,11 @@ if ok then
 	ui2.enable({
 		enable = true,
 		msg = {
+			target = "msg",
 			targets = {
 				[""] = "msg",
 				empty = "cmd",
-				bufwrite = "msg",
+				progress = "msg",
 				confirm = "cmd",
 				echo = "msg",
 				echomsg = "msg",
@@ -23,4 +24,27 @@ if ok then
 			pager = { height = 0.6 },
 		},
 	})
+
+	-- HACK/EXPERIMENTAL: This relies on Neovim's internal UI2 API and may break after updates.
+	if ui2.msg and ui2.msg.set_pos and not ui2._msg_top_right_wrapped then
+		local original_set_pos = ui2.msg.set_pos
+
+		ui2.msg.set_pos = function(target)
+			original_set_pos(target)
+
+			if target == nil or target == "msg" then
+				local win = ui2.wins.msg
+				if vim.api.nvim_win_is_valid(win) then
+					vim.api.nvim_win_set_config(win, {
+						relative = "editor",
+						anchor = "NE",
+						row = 1,
+						col = vim.o.columns - 1,
+					})
+				end
+			end
+		end
+
+		ui2._msg_top_right_wrapped = true
+	end
 end
